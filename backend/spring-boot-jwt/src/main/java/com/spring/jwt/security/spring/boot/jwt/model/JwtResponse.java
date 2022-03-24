e19d9db245dec43414bdbf34f05bc4a83c784640
DevMubeenBhatti <mubeen.bhatti@systemsltd.com>
2022-03-24 18:02:17 +0500

    webSecurityConfig error

---
+++ backend/spring-boot-jwt/src/main/java/com/spring/jwt/security/spring/boot/jwt/model/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.spring.jwt.security.spring.boot.jwt.model;
+import java.io.Serializable;
+public class JwtResponse implements Serializable {
+    private final String jwttoken;
+
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+}

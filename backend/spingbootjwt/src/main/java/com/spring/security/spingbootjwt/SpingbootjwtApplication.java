4b5283a655aaa1e6f9159305e028741e4512f4e3
DevMubeenBhatti <mubeen.bhatti@systemsltd.com>
2022-03-24 11:26:05 +0500

    spring project step

---
+++ backend/spingbootjwt/src/main/java/com/spring/security/spingbootjwt/SpingbootjwtApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.spingbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootjwtApplication.class, args);
+	}
+
+}
---
+++ backend/spingbootjwt/src/test/java/com/spring/security/spingbootjwt/SpingbootjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.spingbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
